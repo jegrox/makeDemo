a46f3eb6aa9a559ab8a351b4599203d5be5f519c
Jorge <jorge.candelaria@gmail.com>
2025-01-18 12:55:52 -0500

    Backup to refresh lin endings

--- hellomake.h
+++ hellomake.h
@@ -1,11 +1,4 @@
 /*
 example include file
 */
-
-void myPrintHelloMake(void
-
-/*
-The compilation won't work because I broke something... whoops!
-
-void myPrintHelloMake(void); might work better. 
-*/
+void myPrintHelloMake(void);
